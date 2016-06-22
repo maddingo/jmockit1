c8e3f76365522218de79563e6d56db21968dbf46
Martin Goldhahn <mgoldhahn@gmail.com>
2016-06-23 00:59:14 +0200

    Add unit test for Xml format

--- coverageTests/test/integrationTests/CoverageXmlTest.java
+++ coverageTests/test/integrationTests/CoverageXmlTest.java
@@ -6,36 +6,100 @@ import mockit.coverage.Configuration;
 import mockit.coverage.XmlFile;
 import mockit.coverage.data.CoverageData;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.HandlerBase;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.ext.DefaultHandler2;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.*;
 import java.io.File;
 import java.io.IOException;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeThat;
 
-public class CoverageXmlTest extends CoverageTest {
+public class CoverageXmlTest extends CoverageTest
+{
    ClassWithFields tested;
 
    @Test
-   public void verifyXmlOutputGenerator() {
+   public void verifyXmlOutputGenerator()
+   {
+      File outputFile = new File("target", "coverage.xml");
+      if (outputFile.exists()) {
+         outputFile.delete();
+      }
       assumeThat(Configuration.getProperty("output"), is(equalTo("xml")));
       assumeThat(Configuration.getProperty("outputDir"), is(equalTo("target")));
+
       CodeCoverage.generateOutput(false);
 
-      File outputFile = new File("target", "coverage.xml");
       assertTrue(outputFile.exists());
+
+      outputFile.delete();
    }
 
    @Test
-   public void verifyXmlFile() throws IOException {
-      File outputFile = File.createTempFile("coverage_", ".xml", new File("target"));
-      outputFile.deleteOnExit();
+   public void verifyXmlFile() throws IOException, ParserConfigurationException, SAXException
+   {
+      File tempFile = File.createTempFile("coverage_", ".xml", new File("target/"));
+      tempFile.deleteOnExit();
 
-      new XmlFile("target", CoverageData.instance()).generate();
+      // We want to override the default output file
+      class TestXmlFile extends XmlFile {
+         public TestXmlFile() {
+            super(tempFile, null, CoverageData.instance());
+         }
+      }
+      new TestXmlFile().generate();
 
-      assertTrue(outputFile.exists());
+      DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+      Document doc = docBuilder.parse(tempFile);
+
+      Element documentElem = doc.getDocumentElement();
+      assertThat(documentElem.getTagName(), is(equalTo("coverage")));
+      assertThat(documentElem.getAttribute("version"), is(equalTo("1")));
+
+      NodeList fileNodeList = documentElem.getElementsByTagName("file");
+      int nboNodes = fileNodeList.getLength();
+      assertTrue(nboNodes > 0);
+
+      // find the file element for the tested class
+      Node testClassNode = null;
+      for (int i = 0; i < nboNodes; i++) {
+         Node fileNode = fileNodeList.item(i);
+         assertTrue(fileNode.getParentNode().isEqualNode(documentElem));
+         Node pathAttr = fileNode.getAttributes().getNamedItem("path");
+         if ("integrationTests/data/ClassWithFields.java".equals(pathAttr.getNodeValue())) {
+            testClassNode = fileNode;
+         }
+      }
+
+      assertThat(testClassNode, is(notNullValue()));
+
+      NodeList linesToCoverList = ((Element) testClassNode).getElementsByTagName("lineToCover");
+      int nboLinesToCover = linesToCoverList.getLength();
+      assertTrue(nboLinesToCover > 0);
+
+      for(int i = 0; i < nboLinesToCover; i++) {
+         Node item = linesToCoverList.item(i);
+         assertTrue(item.getParentNode().isEqualNode(testClassNode));
+         // Does the lineToCover node have a lineNumber and covered attribute?
+         Node lineNumber = item.getAttributes().getNamedItem("lineNumber");
+         assertThat(lineNumber, is(notNullValue()));
+
+         Node covered = item.getAttributes().getNamedItem("covered");
+         assertThat(covered, is(notNullValue()));
+
+         assertThat(covered.getNodeValue(), either(is(equalTo("true"))).or(is(equalTo("false"))));
+      }
    }
 }
